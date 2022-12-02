5de10f90f374b93d0708e2428f66d58e1bdd9ea7
matt <matthewjohncoleman@gmail.com>
2022-12-02 17:00:28 -0600

    refactor day1

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -5,9 +5,7 @@ import java.io.File
 class Day1 : Day(1) {
 
     override fun partOne(): Any {
-        val data = File(javaClass.classLoader.getResource("input_day_1.txt").toURI()).readText()
-
-        val totalCaloriesList = data.split("\n\n")
+        val totalCaloriesList = inputString.split("\n\n")
             .map { it.split("\n").map { it.toInt() }.reduce { acc, s ->  acc + s } }
         val max = totalCaloriesList.max()
 
@@ -15,27 +13,10 @@ class Day1 : Day(1) {
     }
 
     override fun partTwo(): Any {
-        val data = File(javaClass.classLoader.getResource("input_day_1.txt").toURI()).readText()
-
-        val totalCaloriesList = data.split("\n\n")
-            .map { it.split("\n").reduce { acc, s ->  (acc.toInt() + s.toInt()).toString() } }
-            .map { it.toInt() }
+        val totalCaloriesList = inputString.split("\n\n")
+            .map { it.split("\n").map { it.toInt() }.reduce { acc, s -> acc + s } }
         val sumTop3 = totalCaloriesList.sortedDescending().subList(0,3).toList().sum()
 
         return sumTop3.toString()
     }
-
-    /*
-    //            .reduce {acc, s ->  (acc.toInt() + s.toInt()).toString() }
-       val sums = mutableListOf<Int>(0)
-        var activeindex = 0
-        for(it in inputList) {
-            if(it.isEmpty()) {
-                activeindex++
-                sums.add(0)
-            } else {
-                sums[activeindex] = sums[activeindex] + it.toInt()
-            }
-        }
-     */
 }
