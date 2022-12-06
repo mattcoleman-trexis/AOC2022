574ec431255b95a125e6144a2795c3ef8ce472cc
matt <matthewjohncoleman@gmail.com>
2022-12-06 10:30:50 -0600

    refactor 6

--- src/main/kotlin/days/Day6.kt
+++ src/main/kotlin/days/Day6.kt
@@ -14,17 +14,7 @@ class Day6 : Day(6) {
         var markerPosition = 0
         for (i in 0 until inputString.length) {
             val sub = inputString.substring(i, length+i)
-            var hasDuplicate: Boolean = false
-            sub.forEachIndexed { index, c ->
-                if(index == sub.lastIndex) return@forEachIndexed
-                for (x in (index + 1) until sub.length) {
-                    if(sub[index] == sub[x]) {
-                        hasDuplicate = true
-                        break
-                    }
-                }
-            }
-            if (!hasDuplicate) {
+            if (sub.toCharArray().distinct().count() == length) {
                 markerPosition = i + length
                 break
             }
